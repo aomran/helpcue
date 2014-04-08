fd8633f1fd749a851412ce8684e5bc6937c53118
Ahmed Omran <aomra015@gmail.com>
2014-04-08 13:45:32 -0400

    added teacher and student scopes

--- app/models/classroom_user.rb
+++ app/models/classroom_user.rb
@@ -1,4 +1,7 @@
 class ClassroomUser < ActiveRecord::Base
   belongs_to :classroom
   belongs_to :user
+
+  scope :teachers, -> { where(role: 'Admin') }
+  scope :students, -> { where(role: 'User') }
 end
