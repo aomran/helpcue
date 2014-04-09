8a329472f5de77881b5dce068d82c679a75f84f3
Ahmed Omran <aomra015@gmail.com>
2014-04-08 21:57:26 -0400

    being-helped requests now show too

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,6 @@ class Request < ActiveRecord::Base
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
   belongs_to :classroom
 
-  scope :need_help, -> { where(status: STATUS_OPTIONS[0]).order(" updated_at ASC") }
+  scope :need_help, -> { where.not(status: STATUS_OPTIONS[2]).order(" updated_at ASC") }
   STATUS_OPTIONS = ['Waiting', 'Being Helped', 'Done']
 end
