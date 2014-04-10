56c1ce3e8adadda8688f508c5df9aee6fb4fb7ff
Ahmed Omran <aomra015@gmail.com>
2014-04-10 17:07:47 -0400

    fix seeds typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # Creater Users
-teacher1 = User.create(first_name: 'Teacher', last_name: 'One', email: 'teacher1@fakemail.com'. password: '1234', password_confirmation: '1234')
+teacher1 = User.create(first_name: 'Teacher', last_name: 'One', email: 'teacher1@fakemail.com', password: '1234', password_confirmation: '1234')
 
-teacher2 = User.create(first_name: 'Teacher', last_name: 'Two', email: 'teacher2@fakemail.com'. password: '1234', password_confirmation: '1234')
+teacher2 = User.create(first_name: 'Teacher', last_name: 'Two', email: 'teacher2@fakemail.com', password: '1234', password_confirmation: '1234')
 
-student1 = User.create(first_name: 'Student', last_name: 'One', email: 'student1@fakemail.com'. password: '1234', password_confirmation: '1234')
+student1 = User.create(first_name: 'Student', last_name: 'One', email: 'student1@fakemail.com', password: '1234', password_confirmation: '1234')
 
-student2 = User.create(first_name: 'Student', last_name: 'Two', email: 'student2@fakemail.com'. password: '1234', password_confirmation: '1234')
+student2 = User.create(first_name: 'Student', last_name: 'Two', email: 'student2@fakemail.com', password: '1234', password_confirmation: '1234')
 
 # Create Classroom
 sample_classroom = Classroom.new(name: 'Sample Classroom', description: 'This classroom is used for testing and demonstration purposes only.')
