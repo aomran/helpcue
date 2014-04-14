e4c7992ad7eb39f301d759d78aec2727b69850bd
Ahmed Omran <aomra015@gmail.com>
2014-04-14 15:34:25 -0400

    strip # from search

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,7 +2,12 @@ class HashtagsController < ApplicationController
   before_action :get_classroom
 
   def show
-    @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag] || '')
+    if params[:hashtag]
+      params[:hashtag] = params[:hashtag].gsub(/^#/, '')
+    else
+      params[:hashtag] = ''
+    end
+    @hashtag = SimpleHashtag::Hashtag.find_by_name(params[:hashtag])
     @hashtagged = @hashtag.hashtaggables.select { |obj| obj.classroom_id == @classroom.id } if @hashtag
   end
 
