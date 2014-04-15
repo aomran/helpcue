bfd67eacbd1a5cc8789dfa76861df520dadbc462
Ahmed Omran <aomra015@gmail.com>
2014-04-15 14:24:53 -0400

    email invitations form added for inviting students

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,24 @@
+class InvitationsController < ApplicationController
+
+  before_action :get_classroom
+  after_action :verify_authorized
+
+  def create
+    authorize @classroom, :edit?
+    @emails = params[:invitation_emails].split(/,|\n/).map!(&:strip)
+
+    if @emails.any? && valid_emails?
+      InvitationMailer.invite(@emails, @classroom.id).deliver
+      redirect_to edit_classroom_path(@classroom), notice: "Invitations sent."
+    else
+      redirect_to edit_classroom_path(@classroom), alert: "Invalid email format"
+    end
+  end
+
+  private
+  def valid_emails?
+    @emails.each do |email|
+      return false unless /\A[^@]+@[^@]+\z/.match(email)
+    end
+  end
+end
---
+++ test/controllers/invitations_controller_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class InvitationsControllerTest < ActionController::TestCase
+  def setup
+    sign_in users(:teacher1)
+  end
+
+  test "should send email to all recipients" do
+    assert_difference 'ActionMailer::Base.deliveries.size', +1 do
+      post :create, classroom_id: classrooms(:one), invitation_emails: "student@email.com, student2@gmail.com"
+    end
+
+    invite_email = ActionMailer::Base.deliveries.last
+
+    assert_equal ["student@email.com", "student2@gmail.com"], invite_email.to
+    assert_redirected_to edit_classroom_path(classrooms(:one))
+  end
+
+  test "should give error with invalid email formats" do
+    post :create, classroom_id: classrooms(:one), invitation_emails: "student@email.com student2@gmail.com"
+
+    assert_equal "Invalid email format", flash[:alert]
+    assert_redirected_to edit_classroom_path(classrooms(:one))
+  end
+
+  test "should give error with no emails" do
+    post :create, classroom_id: classrooms(:one), invitation_emails: ""
+
+    assert_equal "Invalid email format", flash[:alert]
+    assert_redirected_to edit_classroom_path(classrooms(:one))
+  end
+end
