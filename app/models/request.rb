cc07d5370f50ce1b14e299afcac66ae18d683bf3
Ahmed Omran <aomra015@gmail.com>
2014-05-19 18:39:51 -0400

    remove code that isn't needed

--- app/models/request.rb
+++ app/models/request.rb
@@ -8,7 +8,6 @@ class Request < ActiveRecord::Base
 
   scope :need_help, -> { where.not(status: STATUS_OPTIONS[2]).order("created_at ASC") }
   scope :completed, -> { where(status: STATUS_OPTIONS[2]).order("updated_at DESC") }
-  scope :past_24_hours, -> { where("helped_at > ?", 24.hours.ago) }
 
   STATUS_OPTIONS = ['Waiting', 'Being Helped', 'Done']
 
@@ -59,16 +58,6 @@ class Request < ActiveRecord::Base
   def help_duration
     if helped_at && done_at
       (done_at - helped_at).floor
-    else
-      0
-    end
-  end
-
-  def self.average_waiting_time
-    waiting_times = all.map {|r| r.time_waiting }
-    waiting_times.reject! { |t| t <= 2.seconds }
-    if waiting_times.any?
-      waiting_times.reduce(:+) / waiting_times.length
     else
       0
     end
